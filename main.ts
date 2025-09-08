e2100cabafbe7b1e6853621d44bdd7039fcf7826
Bryan-Nelson-2 <bryan.nelson@stu.ocsb.ca>
2025-09-08 19:56:15 -0400

    Changed ('Hello, World!') - ('Hello, Bryan!')

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Bryan
+ * Created on: Sep 2025
+ * This program shows the text below (Hello, Bryan!)
 */
 
-basic.showString('Hello, World!')
+basic.showString('Hello, Bryan!')
