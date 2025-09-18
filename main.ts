bfb3df0362f780730fc20c97f9c6bb780278aba1
Bryan-Nelson-2 <bryan.nelson@stu.ocsb.ca>
2025-09-17 21:58:09 -0400

    Can now calculate and show perimeter and area

--- main.ts
+++ main.ts
@@ -2,7 +2,11 @@
  *
  * Created by: Bryan
  * Created on: Sep 2025
- * This program shows the text below (Hello, Bryan!)
+ * This program below finds the perimeter and area of a rectangle
 */
 
-basic.showString('Hello, Bryan!')
+basic.showString("A rectangle has dimensions 5 cm & 3 cm.")
+basic.showString("Perimeter: 2(5 + 3) = " + (2 * (5 + 3)) + " cm")
+basic.showString("Area: 5 x 3 = " + (5 * 3) + " cm")
+basic.showIcon(IconNames.Happy)
+basic.pause(100)
